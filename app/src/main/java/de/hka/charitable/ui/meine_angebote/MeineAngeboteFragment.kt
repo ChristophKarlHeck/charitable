d8fbfb877e744a609b21675033a8abeedddee4bf
ChristophKarlHeck <christoph-heck@web.de>
2021-12-11 15:46:09 +0100

    listOwnMealsAdded

--- app/src/main/java/de/hka/charitable/ui/meine_angebote/MeineAngeboteFragment.kt
+++ app/src/main/java/de/hka/charitable/ui/meine_angebote/MeineAngeboteFragment.kt
@@ -4,41 +4,65 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import de.hka.charitable.R
 import de.hka.charitable.databinding.FragmentMeineAngeboteBinding
 
 class MeineAngeboteFragment : Fragment() {
 
-private lateinit var meineAngeboteViewModel: MeineAngeboteViewModel
-private var _binding: FragmentMeineAngeboteBinding? = null
+        private lateinit var meineAngeboteViewModel: MeineAngeboteViewModel
+        private var _binding: FragmentMeineAngeboteBinding? = null
 
-// This property is only valid between onCreateView and
-// onDestroyView.
-private val binding get() = _binding!!
+        // This property is only valid between onCreateView and
+        // onDestroyView.
+        private val binding get() = _binding!!
 
         override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
         ): View? {
-        meineAngeboteViewModel =
-        ViewModelProvider(this).get(MeineAngeboteViewModel::class.java)
+                meineAngeboteViewModel =
+                ViewModelProvider(this).get(MeineAngeboteViewModel::class.java)
 
-        _binding = FragmentMeineAngeboteBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+                _binding = FragmentMeineAngeboteBinding.inflate(inflater, container, false)
+                val root: View = binding.root
 
-        val textView: TextView = binding.textMeineAngebote
-        meineAngeboteViewModel.text.observe(viewLifecycleOwner, Observer {
-        textView.text = it
-        })
-        return root
+
+                return root
         }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+                super.onViewCreated(view, savedInstanceState)
+                val listOwnMeals = view.findViewById<ListView>(R.id.listMyMeals)
+                val adapter = ArrayAdapter<String>(view.context, android.R.layout.simple_list_item_1, this.getFoodListItems())
+                listOwnMeals.adapter = adapter
+
+        }
+
+        private fun getFoodListItems(): Array<String> {
+                val list = arrayOf(
+                        "Cristiano Ronaldo",
+                        "Messi",
+                        "Neymar",
+                        "Isco",
+                        "Hazard",
+                        "Mbappe",
+                        "Hazard",
+                        "Ziyech",
+                        "Suarez"
+                )
+                return list;
+        }
+
 
         override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-        }
+                super.onDestroyView()
+                _binding = null
+                }
         }
--- app/src/main/java/de/hka/charitable/ui/meine_angebote/MeineAngeboteViewModel.kt
+++ app/src/main/java/de/hka/charitable/ui/meine_angebote/MeineAngeboteViewModel.kt
@@ -6,8 +6,8 @@ import androidx.lifecycle.ViewModel
 
 class MeineAngeboteViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
+   /* private val _text = MutableLiveData<String>().apply {
         value = "This is meineAngebote Fragment"
     }
-    val text: LiveData<String> = _text
+    val text: LiveData<String> = _text*/
 }
