45a18bc6a984299cb7f3c84b13541259f95cc1db
ChristophKarlHeck <christoph-heck@web.de>
2021-12-12 12:07:51 +0100

    change date added

--- app/src/main/java/de/hka/charitable/CreateMealActivity.kt
+++ app/src/main/java/de/hka/charitable/CreateMealActivity.kt
@@ -1,12 +1,40 @@
 package de.hka.charitable
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.time.Month
+import java.time.MonthDay
+import java.time.Year
+import java.util.*
 
 class CreateMealActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_meal)
+
+        val imageView = findViewById<ImageView>(R.id.imageView2)
+        imageView.setImageResource(R.drawable.ic_create_meal_spaghetti)
+
+        val changeDateButton = findViewById<Button>(R.id.button)
+        val showDate = findViewById<TextView>(R.id.textView2)
+
+        val sdf = SimpleDateFormat("dd.MM.yyyy")
+        showDate.text = sdf.format(Date())
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        changeDateButton.setOnClickListener{
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{
+                    view, mYear, mMonth, mDay -> showDate.text=("$mDay.$mMonth.$mYear")},year, month, day)
+            dpd.show()
+        }
     }
 }
